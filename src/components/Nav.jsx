7959cec147ce7b1c7845511b838752f216b3721e
Balu248 <balachandran7236@gmail.com>
2025-07-14 16:14:00 +0530

    News api

---
+++ src/components/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div>
+        <nav class="navbar navbar-expand-lg bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <Link class="nav-link active" aria-current="page" to="/">View News</Link>
+        <Link class="nav-link" to="#">Sports News</Link>
+        <Link class="nav-link" to="#">Editorial</Link>
+        <Link class="nav-link disabled" aria-disabled="true">Disabled</Link>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/components/Viewnews.jsx
@@ -0,0 +1,67 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Nav from './Nav'
+
+const Viewnews = () => {
+    const [newsData, xnewsData] = useState(
+        {"status":"ok","totalResults":34,"articles":[]}
+    )
+    const fetchData = () => {
+        axios.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=7c1139f61f624a3abc81786995a761c7").then(
+            (res) => {
+                xnewsData(res.data)
+            }
+        ).catch()
+    }
+    useEffect(() => { fetchData() }, [])
+    return (
+        <div className='bg-primary-subtle p-4 rounded'>
+            <Nav/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-2 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <h1 align="center">The Daily NEWS</h1>
+                            {newsData.articles.map(
+                                (value, index) => {
+                                    return (
+                                        <div className="col col-12 col-sm-12 col-md-2 col-lg-12 col-xl-12 col-xxl-12">
+                                            <div class="card mb-3" >
+                                                <div class="row g-0">
+                                                    <div class="col-md-4">
+                                                        <img src={value.urlToImage} class="img-fluid rounded-start" alt="..."></img>
+                                                    </div>
+                                                    <div class="col-md-8">
+                                                        <div class="card-body">
+                                                            <h5 class="card-title">{value.title}</h5>
+                                                            <h6 class="card-subtitle mb-2 text-body-secondary">{value.name} <b>Author:</b>{value.author}</h6>
+                                                            <p class="card-text">{value.description}</p>
+                                                            <p class="card-text">{value.content}</p>
+                                                            <p class="card-text"><small class="text-body-secondary">Published: {value.publishedAt}</small></p>
+                                                            <a href="#" class="card-link">{value.url}</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+
+
+
+
+                                        </div>
+                                    )
+                                }
+                            )}
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Viewnews
